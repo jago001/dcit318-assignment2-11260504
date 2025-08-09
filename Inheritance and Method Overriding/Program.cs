c53e8f2e38e6b4a63476c94d8679ae734f9a0197
jago001 <jago001@st.ug.edu.gh>
2025-08-09 21:18:07 +0100

    Inheritance and Method Overriding program

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+// Base class Animal
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class Dog
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class Cat
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances of each class
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        // Call MakeSound() on each instance
+        Console.WriteLine("Animal sound:");
+        animal.MakeSound();
+
+        Console.WriteLine("\nDog sound:");
+        dog.MakeSound();
+
+        Console.WriteLine("\nCat sound:");
+        cat.MakeSound();
+    }
+}
