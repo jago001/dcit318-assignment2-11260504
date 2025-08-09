d0a897829b99be2575177acac49cd7fdae18a63f
jago001 <jago001@st.ug.edu.gh>
2025-08-09 21:37:02 +0100

    Interfaces programe

---
+++ Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        Console.WriteLine("Moving the car:");
+        car.Move();
+
+        Console.WriteLine("\nMoving the bicycle:");
+        bicycle.Move();
+    }
+}
